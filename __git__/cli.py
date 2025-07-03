b87ce4daf8b6cf983c04e605a1f2836dab722d36
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-07-03 11:57:00 +0300

    feat: add argument parser

--- __git__/cli.py
+++ __git__/cli.py
@@ -1,2 +1,19 @@
-def main():
-    pass
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(dest="command") # add subcommands and store them in `args.commands`
+    commands.required = True
+
+    init_parser = commands.add_parser("init")
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+def init(args):
+    print("This is the init function!")
+
+if __name__ == "__main__":
+    args = parse_args()
+    args.func(args)
+
