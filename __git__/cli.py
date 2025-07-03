c3da9e486bb7a7f1a016641105a444cb175b6942
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-07-03 13:17:11 +0300

    feat: add git object database

--- __git__/cli.py
+++ __git__/cli.py
@@ -10,11 +10,19 @@ def parse_args():
     init_parser = commands.add_parser("init")
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser("hash-object")
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file")
+
     return parser.parse_args()
 
 def init(args):
     data.init()
     print(f'Initialized empty __git__ repository in {os.getcwd()}')
+
+def hash_object(args):
+    with open(args.file, "rb") as file:
+        print(data.hash_object(file.read()))
 
 if __name__ == "__main__":
     args = parse_args()
