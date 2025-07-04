2a091f83a997948033d644ec10c6ccd7fc5f028e
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-07-04 23:45:37 +0300

    feat: ignore  in write-tree

--- __git__/base.py
+++ __git__/base.py
@@ -5,6 +5,9 @@ def write_tree(directory='.'):
     with os.scandir(directory) as path:
         for entry in path:
             full = os.path.join(directory, entry.name)
+            if is_ignored(full):
+                continue
+
             if entry.is_file(follow_symlinks=False):
                 # TODO : write the file to object store
                 print(full)
@@ -13,3 +16,8 @@ def write_tree(directory='.'):
 
     # TODO: actually create the tree object
 
+def is_ignored(path):
+    ignored_dirs = ['.__git__']
+    if os.path.basename(path) in ignored_dirs:
+        return True
+    return False
