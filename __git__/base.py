73be0489560f741d09d5287f33c0ae62af3994bb
Sergiu Gherasim <sergiugherasim@Sergius-MacBook-Pro.local>
2025-07-04 23:40:08 +0300

    fix: wrong full path in base.py

--- __git__/base.py
+++ __git__/base.py
@@ -4,7 +4,7 @@ import data
 def write_tree(directory='.'):
     with os.scandir(directory) as path:
         for entry in path:
-            full = f"{path}/{entry}" 
+            full = os.path.join(directory, entry.name)
             if entry.is_file(follow_symlinks=False):
                 # TODO : write the file to object store
                 print(full)
